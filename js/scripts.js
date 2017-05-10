84f744e79bbdefb4e02bdbfe612e09ccfbf05c7c
katerina70 <ektrn_e@mail.ru>
2017-05-10 18:36:10 +0300

    Завершаем верстку 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 var link = document.querySelector(".booking-button");
-var popup = document.querySelector(".hotel-book-window");
+		var popup = document.querySelector(".hotel-book-window");
+		var html = document.documentElement;
 
-link.addEventListener("click", function (event) {
-	event.preventDefault();
-	popup.classList.toggle("hotel-book-window-open");
+		html.className = html.className.replace("no-js", "js");
 
+		link.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (popup.classList.contains("hotel-book-window-close")) {
+    popup.classList.add("hotel-book-window-show");
+    popup.classList.remove("hotel-book-window-close");
+  }
+  else {
+    popup.classList.add("hotel-book-window-close");
+    popup.classList.remove("hotel-book-window-show");
+  };
 });
